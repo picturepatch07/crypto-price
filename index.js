88b08bf2cacb7711bcdef6c934589ce48ab6196a
Abhishek Anand <anand2000ys@gmail.com>
2023-03-14 21:42:49 +0530

    api added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const price = require('crypto-price')
 
+const express = require('express')
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 price.getCryptoPrice('USD', 'BTC')
 .then((result) => {
     console.log(result)
